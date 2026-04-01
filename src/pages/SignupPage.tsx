b8b29aa83e2ab34fc60651097d0e36e8d126f16c
jeetestman-creator <jeetestman@gmail.com>
2026-04-01 11:33:18 +0530

    Refactor conditional rendering in SignupPage

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -239,8 +239,7 @@ export default function SignupPage() {
 
             <CardDescription className="text-center">
               {step === 'form'
-                ? 'Join Gold X Usdt and start earning today'
-                : `Enter the 6-digit code sent to ${email}`
+                ? 'Join Gold X Usdt and start earning today' : `Enter the 6-digit code sent to ${email}`
               }
             </CardDescription>
 
